6422540134d9e104dbd366f627f31169d0443b08
0508994 <m.n.milan@hotmail.com>
2017-04-10 21:48:23 +0200

    prvi komit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+
+//var index = require('./routes/index');
+//var user = require('./routes/user')
+
+
+
+var app = express();
+var port = 8000;
+//view engine
+
+//app.set('views',path.join(__dirname,'views'));
+//app.set('view engine','ejs');
+//app.engine('html', require('ejs').renderFile);
+
+//set static folder
+
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//session
+//app.use(session({secret:"password", resave:false, saveUninitialized:true}));
+
+
+//app.use('/', index);
+//app.use('/api', user);
+
+app.listen(port, function(){
+    console.log('Server started on port', + port);
+})
