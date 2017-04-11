9e1ca913328da8ac8fdfb71563935ef90d9e6e3d
0508994 <m.n.milan@hotmail.com>
2017-04-11 04:39:34 +0200

    ++

--- routes/rgapi.js
+++ routes/rgapi.js
@@ -1,11 +1,13 @@
 var express  = require('express');
 var mysql = require('mysql');
 var router  = express.Router();
+var multer  = require('multer');
+var upload = multer({dest:'./public/images/'});
 
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'admin',//milan
-  password : 'admin',//milan
+  user     : 'milan',//milan
+  password : 'milan',//milan
   database : 'mydb'
 });
 
@@ -13,15 +15,30 @@ connection.connect();
 
 
 router.post('/getCity', function(req, res){
-        var name = req.body.cityName;
+        var name = 'Nis';/*req.body.cityName*/;
         console.log(name);
         connection.query('SELECT Name FROM City WHERE Name =?',[name], function (error, results, fields) {
           
         if (error) throw error;
             console.log('The city is: ', results[0]);   
-            return res.json(results[0]);
+            return res.end(JSON.stringify(results[0]));
         });
    
 });
+
+router.post('/imageUpload', upload.single('pic'), function (req, res) {
+     //return res.end(req.file.filename);   // ako nece uradi strinfnfnif kao u CH
+        var takerNick = req.body.takerNick;
+        var placeId = req.body.placeId;
+        var filename = req.file.filename;
+
+        connection.query('INSERT INTO PICTURE (Name, TakerNickname, Place_PlaceId) VALUES(?, ?, ?) ',[fileName, takerNick, filename], function (error, results, fields) {
+          
+        if (error) throw error; 
+            return res.end("Success!");
+        });
+});
+
+
 
 module.exports = router;
