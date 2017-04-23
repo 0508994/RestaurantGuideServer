890f2ef3350117dc3cdeeebb59eaceb05568aef8
milosmladenovic5 <milosml@outlook.com>
2017-04-23 18:18:05 +0200

    j

--- routes/rgapi.js
+++ routes/rgapi.js
@@ -6,28 +6,28 @@ var upload = multer({dest:'./public/images/'});
 
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'milan',//milan
-  password : 'milan',//milan
+  user     : 'admin',//milan
+  password : 'admin',//milan
   database : 'mydb'
 });
 
 connection.connect();
 
 
-router.post('/getCity', function(req, res){
+router.post('/getCityByName', function(req, res){
         var name = 'Nis';/*req.body.cityName*/;
-        console.log(name);
+      
         connection.query('SELECT Name FROM City WHERE Name =?',[name], function (error, results, fields) {
             if (error) throw error;
-            console.log('The city is: ', results[0]);   
+
             return res.end(JSON.stringify(results[0]));
         });
 });
 
 router.post('/getPlaceByName', function(req, res){
-    var name = 'Pleasure';/*req.body.placeName*/
+    var name = req.body.placeName;//'Pleasure';/*req.body.placeName*/
     
-    connection.stringify('SELECT * FROM Place WHERE Name =?',[name],function(error,results,fields){
+    connection.query('SELECT * FROM Place WHERE Name =?',[name],function(error,results,fields){
             if(error) throw error;
             
             return res.end(JSON.stringify(results[0]));
@@ -39,7 +39,7 @@ router.post('/getPlaceByName', function(req, res){
 router.post('/getPlaceById', function(req, res){
     var placeId = 'Pleasure';/*req.body.placeName*/
     
-    connection.stringify('SELECT * FROM Place WHERE PlaceId =?',[placeId],function(error,results,fields){
+    connection.query('SELECT * FROM Place WHERE PlaceId =?',[placeId],function(error,results,fields){
             if(error) throw error;
             
             return res.end(JSON.stringify(results[0]));
@@ -49,7 +49,7 @@ router.post('/getPlaceById', function(req, res){
 router.post('getCommentById', function(req,res){
     var commentId = req.body.commentId;
 
-    connection.stringify('SELECT * FROM Comment WHERE CommentId = ?', [commentId], function(error,results,fields){
+    connection.query('SELECT * FROM Comment WHERE CommentId = ?', [commentId], function(error,results,fields){
             if(error) throw error;
 
             return res.end(JSON.stringify(results[0]));
@@ -57,9 +57,9 @@ router.post('getCommentById', function(req,res){
 });
 
 router.post('getRestaurantsByCoucine', function (req, res){
-    var coucine = req.body.coucine;
+    var coucine = req.body.coucine;//italijanska, spanska, kineska, francuska, engleska, ruska, balkanska
 
-    connection.stringify('SELECT * FROM Place WHERE Coucine = ?', [coucine], function(error, results,fields){
+    connection.query('SELECT * FROM Place WHERE Coucine = ?', [coucine], function(error, results,fields){
         if (error) throw error;
 
         return res.end(JSON.stringify(results));
@@ -70,7 +70,7 @@ router.post('getRestaurantsByCoucine', function (req, res){
 router.post('getPlacesByLiveMusic', function (req, res){
     var liveMusicBool = req.body.liveMusic; //prosledjujemo YES ili NO, kako ce i stajati u bazi
 
-    connection.stringify('SELECT * FROM Place WHERE LiveMusic = ?', [liveMusicBool], function(error, results, fields){
+    connection.query('SELECT * FROM Place WHERE LiveMusic = ?', [liveMusicBool], function(error, results, fields){
         if (error) throw error;
 
         return res.end(JSON.stringify(results));
@@ -80,7 +80,7 @@ router.post('getPlacesByLiveMusic', function (req, res){
 router.post ('getPlacesByType', function (req, res){
     var placeType = req.body.placeType; //'restoran', 'kafana', 'kafic', klub
 
-    connection.stringify('SELECT * FROM Place WHERE PlaceType = ?', [placeType], function(error, results, fields){
+    connection.query('SELECT * FROM Place WHERE PlaceType = ?', [placeType], function(error, results, fields){
         if (error) throw error;
 
         return res.end(JSON.stringify(results));
@@ -90,7 +90,18 @@ router.post ('getPlacesByType', function (req, res){
 router.post('getCommentsByPlaceId', function(req, res){
     var placeId = req.body.placeId;
 
-    connection.stringify('SELECT * FROM Comment WHERE PlaceId = ?',[placeId], function( error,results, fields){
+    connection.query('SELECT * FROM Comment WHERE PlaceId = ?',[placeId], function( error,results, fields){
+        if(error) throw error;
+
+        return res.end(JSON.stringify(results));
+    });
+});
+
+router.post('getPlaceByCoucineAndMusic', function(req, res){
+    var coucine = req.body.coucine;
+    var music = req.body.liveMusic;
+
+    connection.query('SELECT * FROM Place WHERE Coucine = ? AND LiveMusic = ?', [coucine,music], function(error, results, fields){
         if(error) throw error;
 
         return res.end(JSON.stringify(results));
