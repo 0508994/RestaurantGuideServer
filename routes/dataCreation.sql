1708285cecec2f2999229b59eb2bc86d0fcb38ed
0508994 <m.n.milan@hotmail.com>
2017-04-23 22:25:31 +0200

    datacreation.sql

---
+++ routes/dataCreation.sql
@@ -0,0 +1,17 @@
+INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewsNumber, GrossScore, LiveMusic, PlaceType, Coucine, Latitude, Longitude, PhoneNumber, Adress, WorkingHours) 
+VALUES (1, "Biser", 1, NULL, 0, 0, "Yes", "Kafana", "Balkanska", 43.317759, 21.8954513, "+381618079418", "Koste Stamenkovića 1", "7AM - 12AM");
+
+
+INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewsNumber, GrossScore, LiveMusic, PlaceType, Coucine, Latitude, Longitude, PhoneNumber, Adress, WorkingHours) 
+VALUES (2, "Galija", 1, NULL, 0, 0, "Yes", "Kafana", "Balkanska", 43.3180417, 21.8960932, "+381618079418", "Nikole Pašića 35", "10AM - 12AM");
+
+
+INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewsNumber, GrossScore, LiveMusic, PlaceType, Coucine, Latitude, Longitude, PhoneNumber, Adress, WorkingHours) 
+VALUES (3, "Dušanov Konak", 1, NULL, 0, 0, "Yes", "Kafana", "Balkanska", 43.3180427, 21.9060469, "+381618079418", "Cara Dušana 124b", "0 - 24h");
+
+
+INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewsNumber, GrossScore, LiveMusic, PlaceType, Coucine, Latitude, Longitude, PhoneNumber, Adress, WorkingHours) 
+VALUES (4, "Riva", 1, NULL, 0, 0, "No", "Restoran", "Italijanska", 43.3252738, 21.9015953, "+381618079418", "Kej 29. decembar 18b", "12:30PM - 12AM");
+
+
+--
