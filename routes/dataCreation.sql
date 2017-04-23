b72ad0506734a795f1b04e5132c9ef05ce1fb156
milosmladenovic5 <milosml@outlook.com>
2017-04-23 22:44:55 +0200

    s

--- routes/dataCreation.sql
+++ routes/dataCreation.sql
@@ -5,7 +5,7 @@ INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewersNumber, GrossScore,
 VALUES (2, "Galija", 1, NULL, 0, 0, "Yes", "Kafana", "Balkanska", 43.3180417, 21.8960932, "+381618079418", "Nikole Pasica 35", "10AM - 12AM");
 
 INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewersNumber, GrossScore, LiveMusic, PlaceType, Coucine, Latitude, Longitude, PhoneNumber, Address, WorkingHours) 
-VALUES (3, "Dušanov Konak", 1, NULL, 0, 0, "Yes", "Kafana", "Balkanska", 43.3180427, 21.9060469, "+381618079418", "Cara Dusana 124b", "0 - 24h");
+VALUES (3, "Dusanov Konak", 1, NULL, 0, 0, "Yes", "Kafana", "Balkanska", 43.3180427, 21.9060469, "+381618079418", "Cara Dusana 124b", "0 - 24h");
 
 INSERT INTO PLACE (PlaceId, Name, CityId, PhotoURL, ReviewersNumber, GrossScore, LiveMusic, PlaceType, Coucine, Latitude, Longitude, PhoneNumber, Address, WorkingHours) 
 VALUES (4, "Riva", 1, NULL, 0, 0, "No", "Restoran", "Italijanska", 43.3252738, 21.9015953, "+381618079418", "Kej 29. decembar 18b", "12:30PM - 12AM");
