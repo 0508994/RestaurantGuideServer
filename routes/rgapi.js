1370589e7e127ed54c9ec546c51d977af85288d1
0508994 <m.n.milan@hotmail.com>
2017-12-06 18:31:29 +0100

    clean_up

--- routes/rgapi.js
+++ routes/rgapi.js
@@ -2,8 +2,6 @@ var express  = require('express');
 var mysql = require('mysql');
 var router  = express.Router();
 var multer  = require('multer');
-//var googleMapsClient = require('@google/maps').createClient({key:'AIzaSyAwhkF547Hfm-wh-EO2M_MQ0UFpomkYIpw'});
-//var googleDistance = require('google-distance');
 
 var upload = multer({dest:'./public/images/'});
 
@@ -12,15 +10,13 @@ var connection = mysql.createConnection({
   user     : 'sql11181900',
   password : '5rxsEQXiKn',
   database : 'sql11181900'
-  //database : 'mydb'
 });
 
 connection.connect();
 
 
 router.post('/getCityByName', function(req, res){
-        var name = 'Nis';
-        //var name = req.body.cityName;
+        var name = req.body.cityName;
         console.log("called");
         connection.query('SELECT Name FROM City WHERE Name =?',[name], function (error, results, fields) {
             if (error) throw error;
@@ -31,9 +27,9 @@ router.post('/getCityByName', function(req, res){
 
 
 router.post('/getNearbyPlaces', function(req, res){
-    var distance = req.body.distance;//zadata udaljenost od strane korisnika
+    var distance = req.body.distance;
     console.log(distance);
-    var lat = req.body.location.latitude;//trenutna lokacija korisnika
+    var lat = req.body.location.latitude;
     var long = req.body.location.longitude;
 
     console.log(lat);
@@ -46,15 +42,6 @@ router.post('/getNearbyPlaces', function(req, res){
 
         for (var i=0; i<results.length; i++)
         {
-            //  googleDistance.get({index:1, origin:JSON.stringify(lat+','+long), destination: JSON.stringify(results[i].Latitude+','+results[i].Longitude)}, function(err, data){
-            //         if (err) return error;
-
-            //         dist = data.distance;
-            //         if(dist<distance)
-            //         {
-            //             array.push(results[i]);
-            //         }
-            // });
             dist =  getDistanceFromLatLonInKm(lat,long,results[i].Latitude, results[i].Longitude);
             if(dist<distance)
             {
@@ -148,7 +135,6 @@ router.post('/getPhotosInformations', function(req, res){
 
 router.post('/createComment', function(req, res){
     var review = req.body.review;
-    //var timestamp = (new Date()).toISOString().substring(0, 19).replace('T', ' ');
 
     connection.query('INSERT INTO Comment(Nickname, Text, Rating, Timestamp, PlaceId ) VALUES(?, ?, ?, now(), ?)', [review.nickname, review.comment, review.rating, review.placeId], function(error, results, fields){
         if(error) throw error;
@@ -168,7 +154,6 @@ router.post('/imageUpload', upload.single('pic'), function (req, res) {
         console.log("PlaceId is " + req.body.placeId);
         var placeId = req.body.placeId;
         var filename = 'images/'+ req.file.filename;
-        //var timestamp = (new Date()).toISOString().substring(0, 19).replace('T', ' ');
 
         connection.query('INSERT INTO Picture (Name, Place_PlaceId, Timestamp) VALUES (?, ?, now()) ',[filename, placeId], function (error, results, fields)
         {
--- server.js
+++ server.js
@@ -5,38 +5,19 @@ var multer  = require('multer');
 var mysql = require('mysql');
 var cors = require('cors');
 
-
-//var index = require('./routes/index');
 var rgapi = require('./routes/rgapi');
-
-
 
 var app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 var port = 8000;
-//view engine
+
 
 app.set('port', (process.env.PORT || port));
 
-//app.set('views',path.join(__dirname,'views'));
-//app.set('view engine','ejs');
-//app.engine('html', require('ejs').renderFile);
-
-//set static folder
-
-
-//body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//app.use(express.static(path.join(__dirname, 'public')));
-
-//session
-//app.use(session({secret:"password", resave:false, saveUninitialized:true}));
-
-
-//app.use('/', index);
 app.use('/api', rgapi);
 
 app.listen(app.get('port'), function() {
